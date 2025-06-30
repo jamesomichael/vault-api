db1ea2c0c42ea602eec4bfd10219c50c57adbfc6
James Michael <jomichael1994@gmail.com>
2025-06-30 17:10:52 +0100

    add jest

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	collectCoverage: true,
+	coverageDirectory: 'coverage',
+	collectCoverageFrom: [
+		'src/**/*.{ts,tsx}',
+		'!src/**/*.d.ts',
+		'!src/**/index.ts',
+	],
+};
